a4017ab6e05fff5a89e0bd776c9ea569197ac1da
pathas <pathas@naver.com>
2020-05-28 17:46:45 +0900

    Automatic Scripts, Copy Github Code

--- client.js
+++ client.js
@@ -1,16 +1,22 @@
-import "babel-core/register";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+/*
+ * Learning Isomorphic Web Application Development
+ * Copyright © 2015 Konstantin Tarkus, Packt Publishing
+ * All rights reserved.
+ */
 
-const run = () => {
-  ReactDOM.hydrate(<App />, document.getElementById("app"));
-};
+import 'babel-core/register';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const loadedStates = ["complete", "loaded", "interactive"];
+function run() {
+  ReactDOM.hydrate(<App />, document.getElementById('app'));
+}
+
+const loadedStates = ['complete', 'loaded', 'interactive'];
 
 if (loadedStates.includes(document.readyState) && document.body) {
   run();
 } else {
-  window.addEventListener("DOMContentLoaded", run, false);
+  window.addEventListener('DOMContentLoaded', run, false);
 }
--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-import path from "path";
-import express from "express";
-import React from "react";
-import ReactDOM from "react-dom/server";
-import App from "./components/App";
-import Html from "./components/Html";
+/*
+ * Learning Isomorphic Web Application Development
+ * Copyright © 2015 Konstantin Tarkus, Packt Publishing
+ * All rights reserved.
+ */
+
+import 'babel-core/register';
+import path from 'path';
+import express from 'express';
+import React from 'react';
+import ReactDOM from 'react-dom/server';
+import Html from './components/Html';
+import App from './components/App';
 
 const server = express();
 const port = process.env.PORT || 3000;
 
-server.use(express.static(path.join(__dirname, "public")));
+server.use(express.static(path.join(__dirname, 'public')));
 
-server.get("*", (req, res) => {
+server.get('*', (req, res) => {
   const body = ReactDOM.renderToString(<App />);
-  const html = ReactDOM.renderToString(
-    <Html
-      title="My app"
-      description="Isomorphic web application sample"
-      body={body}
-    />
-  );
-  res.send(`<!doctype html>
-  ${html}`);
+  const html = ReactDOM.renderToStaticMarkup(<Html
+    title="My App"
+    description="Isomorphic web application sample"
+    body={body} />);
+  res.send('<!doctype html>\n' + html);
 });
 
-server.listen(port, () => {
-  console.log(`Node.js server is listening at http://localhost:${port}`);
-});
+server.listen(port, () => console.log(
+  `Node.js server is listening at http://localhost:${port}/`
+));
